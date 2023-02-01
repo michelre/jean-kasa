4e625c790d0b0420e84aacd3425f3fbd36a8361e
jeanchaneto <jeanchaneto@gmail.com>
2023-02-01 07:41:45 +0100

    Carousel logic

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import arrowBack from '../assets/arrow_back_ios-24px 1.png';
+import arrowNext from '../assets/arrow_forward_ios-24px 1.png';
 
 
-export const Carousel = () => {
+export const Carousel = ({ pictures, title, }) => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const handlePrev = () => {
+    setCurrentIndex(currentIndex === 0 ? pictures.length - 1 : currentIndex - 1)
+  }
+
+  const handleNext = () => {
+    setCurrentIndex(currentIndex === pictures.length - 1 ? 0 : currentIndex + 1)
+  }
+
   return (
-    <h2>Carousel</h2>
+    <div className="carousel">
+      {pictures.map((picture, index, id) => index === currentIndex && (
+        <div className="picture-container" key={id}>
+          <img src={picture} alt={title} />
+        </div>
+      ))}
+      {currentIndex !== 0 && pictures.length > 1 && (
+        <img src={arrowBack} alt="Previous arrow icon" onClick={handlePrev}/>
+      )}
+      {currentIndex !== pictures.length - 1 && pictures.length > 1 && (
+        <img src={arrowNext} alt="Next arrow icon" onClick={handleNext}/>
+      )}
+    </div>
   )
 }
