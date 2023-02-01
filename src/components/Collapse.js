e6e7cd98dda1bafa7c47b03017b18354c87a8c37
jeanchaneto <jeanchaneto@gmail.com>
2023-02-01 10:28:21 +0100

    Implment collapse html and logic

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import arrowUp from "../assets/arrow_up.png";
+import arrowDown from "../assets/arrow_down.png";
 
 
-export const Collapse = () => {
+export const Collapse = ({ title, content }) => {
+
+  const [open, setOpen] = useState(false);
+
+  let collapseContent;
+  if (typeof content === "string") {
+    collapseContent = content;
+  } else if (Array.isArray(content)) {
+    collapseContent = content.map((item) => {
+      return (
+          <p key={item}>{item}</p>
+      )
+    });
+  }
+
+  const toggle = () => {
+    setOpen(!open);
+  }
+
   return (
-    <div>Collapse</div>
+    <div>
+
+      <button onClick={toggle}>
+        {title}
+        {open && <img src={arrowUp} alt="Arrow up" />}
+        {!open && <img src={arrowDown} alt="Arrow down" />}
+      </button>
+
+      {open && typeof content === "string" && <div>
+        <p>{collapseContent}</p>
+      </div>}
+
+      {open && Array.isArray(content) && <div>
+        {collapseContent}
+      </div> }
+    </div>
   )
 }
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,7 +1,20 @@
+import filledStar from "../assets/filled_star.png";
+import emptyStar from "../assets/empty_star.png";
 
+export const Rating = ({ rating }) => {
+  const ratingStars = [];
 
-export const Rating = () => {
+  for (let i = 0; i < rating; i++) {
+    ratingStars.push(<img src={filledStar} alt="Filled star" key={i + "filled"} />);
+  }
+
+  for (let j = 0; j < 5 - rating; j++) {
+    ratingStars.push(<img src={emptyStar} alt="Filled star" key={j + "empty"} />);
+  }
+
   return (
-    <div>rating</div>
+    <div className="rating">
+      {ratingStars}
+    </div>
   )
 }
